fd3358963a7e51b2c69aab76f1bd489bbe3ae999
amirm@student.uia.no <amirm@student.uia.no>
2024-01-25 17:14:59 +0100

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkTf_fXWL-AgOlDZpwgEouE9lAudQVDLk",
+    authDomain: "ikt205-assignment1.firebaseapp.com",
+    projectId: "ikt205-assignment1",
+    storageBucket: "ikt205-assignment1.appspot.com",
+    messagingSenderId: "578049399692",
+    appId: "1:578049399692:web:0c682a8c60759878c1d4a9"
+};
+
+
+const app = initializeApp(firebaseConfig);
