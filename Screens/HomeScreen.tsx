8ea89b672bf3004f740e81adc627cd2a4baa21e0
Arle Vaara <ajvaara@uia.no>
2024-02-21 12:19:53 +0100

    Hello, World!

---
+++ Screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+import {styles} from "../App";
+6
+export default function HomeScreen()
+{
+    return (
+        <View style={styles.container}>
+            <Text>Hello, World!</Text>
+        </View>
+    )
+}
