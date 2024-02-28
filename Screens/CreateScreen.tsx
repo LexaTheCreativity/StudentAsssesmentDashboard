2b82dd0977d025e9858e8708252ab9b23baf8900
Arle Vaara <ajvaara@uia.no>
2024-02-28 11:14:05 +0100

    Create funksjon ferdig

--- Screens/CreateScreen.tsx
+++ Screens/CreateScreen.tsx
@@ -1,15 +1,33 @@
 import {Pressable, Text, TextInput, View} from "react-native";
 import {StudentType, StudentTypeDefault} from "../studentType";
-import {SetStateAction, useEffect, useState} from "react";
+import {useState} from "react";
 import {styles} from "../styles";
+import {doc, setDoc} from 'firebase/firestore';
+import {db} from '../firebaseConfig';
 
 export default function CreateScreen({navigation, route}) {
     const [student, setStudent] = useState<StudentType>(StudentTypeDefault);
 
-    const handleAdd = () => {
-        console.log('Saved student DOB: ', student.DOB.toLocaleString());
+    const handleAdd = async () => {
+        try
+        {
+            await setDoc(doc(db, 'students2', '3'), {
+                DOB: student.DOB,
+                Grade: student.Grade,
+                Score: student.Score,
+                classID: student.classID,
+                className: student.className,
+                fName: student.fName,
+                lName: student.lName
+            });
+            console.log('Added student: ', student);
+        }
+        catch (error)
+        {
+            console.log('Error, could not add student: ', student);
+        }
 
-        navigation.navigate('Students');
+        navigation.goBack();
     };
 
     const handleNumericInput = (text: string) => {
@@ -62,6 +80,13 @@ export default function CreateScreen({navigation, route}) {
                 placeholderTextColor={'#f0ca84'}
                 value={student.Score.toString()}
                 onChangeText={(text) => handleNumericInput(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Grade"
+                placeholderTextColor={'#f0ca84'}
+                value={student.Grade}
+                onChangeText={(text) => setStudent({...student, Grade: text})}
             />
             <Pressable style={styles.box} onPress={handleAdd}>
                 <Text style={styles.text}>Submit</Text>
